a26634e41e63b69b54a81b172ba7fbbc0e56f281
Olivier R.Measson <olivierberlin1998@hotmail.de>
2022-02-17 11:03:58 +0100

    copied existing code in folder

---
+++ Develop/client/src/js/install.js
@@ -0,0 +1,19 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  butInstall.style.visibility = 'visible';
+  // TODO: Implement a click event handler on the `butInstall` element
+  butInstall.addEventListener('click', () => {
+    event.prompt();
+    console.log('Asking to install the app');
+  });
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  butInstall.style.visibility = 'hidden';
+  console.log('Installed the app already');
+});
